12d35f595d5715d98cdcbc4a8440943ef4aac8cc
Summit2 <pinkslime987@gmail.com>
2023-11-27 14:53:39 +0300

    23233

--- src/components/CargoList.tsx
+++ src/components/CargoList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface CargoItem {
   pk: number;
@@ -15,22 +16,71 @@ const CargoList = () => {
   const [data, setData] = useState<CargoItem[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const fetchData = async () => {
+    try {
+      let url = "http://localhost:8000/cargo/";
+
+      // Append filter to the URL if it's provided
+      // if (filter) {
+      //   url += `?filter=${filter}`;
+      // }
+
+      const response = await axios.get(url);
+      setData(response.data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("An error occurred while fetching data.");
+
+      // If there's an error, try fetching from local data.json after 5 seconds
+      setTimeout(async () => {
+        try {
+          const response = await axios.get('my-app/src/components/data.json');
+          setData(response.data.data);
+          // console.log(jsonData)
+          
+
+          setLoading(false);
+          setError(null);
+        } catch (localError) {
+          console.error("Error fetching local data:", localError);
+          setLoading(false);
+          setError("An error occurred while fetching local data.");
+        }
+      }, 4000);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleFilterChange = (filter: string) => {
+    // Perform a PUT request to update the filter on the server
+    // Adjust the endpoint and payload accordingly
+    console.log('filter changed');
+
+    axios.put("http://localhost:8000/cargo/", null, {
+      params: { filter },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        // After the PUT request is successful, trigger the GET request
+        setData(response.data.data);
+        // Update the URL with the selected filter
+        navigate(`/cargo?filter=${filter}`);
+      })
+      .catch((error) => {
+        console.error("Error updating filter:", error);
+        // Handle the error, e.g., show a notification to the user
+      });
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:8000/cargo/");
-        setData(response.data.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError("An error occurred while fetching data.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchData();
-  }, []);
+  }, [location.search]);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -39,6 +89,8 @@ const CargoList = () => {
   if (error) {
     return <p>{error}</p>;
   }
+
+
 
   return (
     <ul className='card-list'>
--- src/components/NaviBar.tsx
+++ src/components/NaviBar.tsx
@@ -3,12 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link, BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function NavigationAndSearchBar() {
   const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate(); // Get the navigate function from react-router-dom
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -20,47 +21,51 @@ function NavigationAndSearchBar() {
     console.log("Submitted value:", inputValue);
   };
 
+  const handleFilter = (filter: string) => {
+    // Update the URL with the selected filter
+    navigate(`/cargo?filter=${filter}`);
+  };
+
   return (
-    // <Router>
-      <Container>
-        <Row>
-          <Col>
-            <Navbar bg="white" expand="lg">
+    <Container>
+      <Row>
+        <Col>
+        <Navbar bg="white" expand="lg">
               <Navbar.Brand as={Link} to="/cargo">Выбор грузов </Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                   <Nav.Link as={Link} to="/cargo">Главная</Nav.Link>
-                  <NavDropdown title="Груз" id="basic-nav-dropdown">
-                    <NavDropdown.Item as={Link} to="/cargo/12">Обеденный набор 1</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/cargo/14">Запасная одежда</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/cargo/15">Обеденный набор</NavDropdown.Item>
+                  <NavDropdown title="Фильтрация" id="basic-nav-dropdown">
+                    {/* <NavDropdown.Item as={Link} to="/cargo/12">Обеденный набор 1</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/cargo/14">Запасная одежда</NavDropdown.Item> */}
+                    <NavDropdown.Item onClick={() => handleFilter('weight')}>По весу</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => handleFilter('name')}>По названию</NavDropdown.Item>
+                    {/* <NavDropdown.Item onClick={() => handleFilter('weight')}>По весу</NavDropdown.Item> */}
                     
                   </NavDropdown>
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
-          </Col>
-          <Col>
-            <div className="font-ordinary">
-              <form onSubmit={handleSubmit}>
-                {/* <label htmlFor="text">Поиск по названию</label> */}
-                <input
-                  id="text"
-                  name="good_item"
-                  type="text"
-                  className="search-bar"
-                  value={inputValue}
-                  onChange={handleChange}
-                  placeholder="Введите название товара"
-                />
-                <input type="submit" value="Поиск" className="search-button" />
-              </form>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    // </Router>
+        </Col>
+        <Col>
+          <div className="font-ordinary">
+            <form onSubmit={handleSubmit}>
+              <input
+                id="text"
+                name="good_item"
+                type="text"
+                className="search-bar"
+                value={inputValue}
+                onChange={handleChange}
+                placeholder="Введите название товара"
+              />
+              <input type="submit" value="Поиск" className="search-button" />
+            </form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
